1b8fc33592f92f1c85dd41e21accbd17af184170
rsantosluan <rs.luands@gmail.com>
2022-08-13 07:41:57 -0300

    First commit

---
+++ notebooks/SRLib/Utilities.py
@@ -0,0 +1,23 @@
+from sklearn.model_selection import StratifiedShuffleSplit
+class graph(  ):
+    
+    ###Values ​​in the bar graph
+    def plot_values_vbar( ax ):
+        for i in ax.patches:
+            if i == 0:
+                None
+            else:
+                ax.annotate(
+                #Texto a ser plotado
+                round(i.get_height()),
+                #Posição horizontal
+                (i.get_x() + i.get_width() /2, i.get_height() + i.get_height() /80 ),
+                ha='center' ,   
+                color='black',
+                fontsize=15
+    );  
+        return None
+    
+
+
+    ###-
